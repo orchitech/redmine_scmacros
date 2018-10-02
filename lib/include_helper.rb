8ee594c8f60e73b6218f1268b4f96e0425ef45f6
Vojtěch Srdečný <vojtech.srdecny@orchitech.cz>
2018-10-02 18:05:03 +0200

    Change tests so they use redmine's fixtures and infrastucture. Add a test for correct creation of links to files in repo.

--- lib/include_helper.rb
+++ lib/include_helper.rb
@@ -1,6 +1,5 @@
 class IncludeHelper
 
-
   # Link structure is as follows below. {} braces denotes optional parts, if they're not present, a default value is assumed.
   # That means the current main repository or the latest revision on current branch.
   # project_name/repository/{repo_name}/{revisions/revision_hash}/entry/file_path
